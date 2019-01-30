2292a0a81639b29b15153b8232448d7eec626f41
web-labo <abokan.aime@gmail.com>
2019-01-30 01:48:58 +0000

    seeding for product and review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Model\Product;
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
         //
+        'product_id' => function(){
+        return Product::all()->random();
+        },
+        'costumer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
