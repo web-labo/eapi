6092cc385530f9d580ac508a66f439af8f1c0c95
web-labo <abokan.aime@gmail.com>
2019-02-03 13:02:48 +0000

    Review Resource and Product pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
     public function index()
     {
 
-        return  ProductCollection::collection(Product::All());
+        return  ProductCollection::collection(Product::paginate(20));
     }
 
     /**
